a947000d817f20239a0a6479e5c877f9b60c9d24
onepoint <dudarevga@crossgroup.su>
2025-10-05 13:12:54 +0600

    languagedetector added

--- frontend/src/i18n.js
+++ frontend/src/i18n.js
@@ -1,5 +1,6 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
 
 import translationEN from './locales/en/translation.json';
 import translationFR from './locales/fr/translation.json';
@@ -19,10 +20,11 @@ const resources = {
 };
 
 i18n
+  .use(LanguageDetector)
   .use(initReactI18next) // passes i18n down to react-i18next
   .init({
     resources,
-    lng: 'en', // default language
+    fallbackLng: 'en', // fallback language if user language is not available
 
     keySeparator: false, // we do not use keys in form messages.welcome
 
